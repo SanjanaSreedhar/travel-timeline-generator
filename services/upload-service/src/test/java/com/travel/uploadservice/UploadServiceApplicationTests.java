c75482949d3775d52fc54b5fdd1a8fe66f1458dc
Sanjana Govarthan <ssm@celum.com>
2025-12-10 22:13:57 +0530

    Adding project skeleton

---
+++ services/upload-service/src/test/java/com/travel/uploadservice/UploadServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.travel.uploadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
